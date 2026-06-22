9c1044efd69325d9323044bdb5dd5f93d73a3aea
Nelson Alejandro <u202310950@upc.edu.pe>
2026-06-22 01:11:06 -0500

    Agrega dominio de Vercel al CORS

--- empleadospruebatecnica/src/main/java/com/empresa/empleados/Config/SecurityConfig.java
+++ empleadospruebatecnica/src/main/java/com/empresa/empleados/Config/SecurityConfig.java
@@ -69,7 +69,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://recursos-humanos.vercel.app"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:4200",
+                "https://recursos-humanos-7etrcdhu1-nelson-alejandro1.vercel.app"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
